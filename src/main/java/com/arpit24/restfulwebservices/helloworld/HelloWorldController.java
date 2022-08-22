2dd317798a81de2de01db237e44ddfceaf482e82
Arpit Jain <arpit@dream11.com>
2022-08-22 18:32:38 +0530

    add localisation support

--- src/main/java/com/arpit24/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/arpit24/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,12 +1,16 @@
 package com.arpit24.restfulwebservices.helloworld;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
     public String helloWorld() {
@@ -15,6 +19,12 @@ public class HelloWorldController {
 
     @GetMapping("/hello-world-bean")
     public HelloWorldBean helloWorldByBean() {
-        return new HelloWorldBean("Choospa Bean! OMG");
+        return new HelloWorldBean("Choose Bean! OMG");
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+
+        return messageSource.getMessage("good.morning.message", null,"Default message", locale);
     }
 }
